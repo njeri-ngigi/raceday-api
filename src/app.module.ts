9bef345bf34864edaf83b1f0fe5d94a7d0070cca
Njeri Ngigi <njery.ngigi@gmail.com>
2025-05-22 21:06:37 +0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './core/database/database.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { JwtModule } from './modules/jwt/jwt.module';
+import { UserModule } from './modules/user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    UserModule,
+    AuthModule,
+    JwtModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/core/constants/index.ts
@@ -0,0 +1,7 @@
+export const DEVELOPMENT = 'development';
+export const TEST = 'test';
+export const PRODUCTION = 'production';
+
+export const SEQUELIZE = 'SEQUELIZE';
+export const USER_REPOSITORY = 'USER_REPOSITORY';
+export const ROLE_REPOSITORY = 'ROLE_REPOSITORY';
